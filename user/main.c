9e52cd91938ba50c4ba4a4b8d496672dee74cf6f
tobowl <1532602400@qq.com>
2024-03-27 16:52:30 +0800

    task create

--- user/main.c
+++ user/main.c
@@ -22,49 +22,7 @@
  * https://www.FreeRTOS.org
  * https://github.com/FreeRTOS
  *
- */
-
-/*
- * Creates all the demo application tasks, then starts the scheduler.  The WEB
- * documentation provides more details of the standard demo application tasks.
- * In addition to the standard demo tasks, the following tasks and tests are
- * defined and/or created within this file:
- *
- * "Fast Interrupt Test" - A high frequency periodic interrupt is generated
- * using a free running timer to demonstrate the use of the
- * configKERNEL_INTERRUPT_PRIORITY configuration constant.  The interrupt
- * service routine measures the number of processor clocks that occur between
- * each interrupt - and in so doing measures the jitter in the interrupt timing.
- * The maximum measured jitter time is latched in the ulMaxJitter variable, and
- * displayed on the LCD by the 'Check' task as described below.  The
- * fast interrupt is configured and handled in the timertest.c source file.
- *
- * "LCD" task - the LCD task is a 'gatekeeper' task.  It is the only task that
- * is permitted to access the display directly.  Other tasks wishing to write a
- * message to the LCD send the message on a queue to the LCD task instead of
- * accessing the LCD themselves.  The LCD task just blocks on the queue waiting
- * for messages - waking and displaying the messages as they arrive.  Messages
- * can either be a text string to display, or an instruction to update MEMS
- * input.  The MEMS input is used to display a ball that can be moved around
- * LCD by tilting the STM32 Primer.  45% is taken as the neutral position.
- *
- * "Check" task -  This only executes every five seconds but has the highest
- * priority so is guaranteed to get processor time.  Its main function is to
- * check that all the standard demo tasks are still operational.  Should any
- * unexpected behaviour within a demo task be discovered the 'check' task will
- * write an error to the LCD (via the LCD task).  If all the demo tasks are
- * executing with their expected behaviour then the check task writes PASS
- * along with the max jitter time to the LCD (again via the LCD task), as
- * described above.
- *
- * Tick Hook - A tick hook is provided just for demonstration purposes.  In
- * this case it is used to periodically send an instruction to updated the
- * MEMS input to the LCD task.
- *
- */
-
-/* CircleOS includes.  Some of the CircleOS peripheral functionality is
-utilised, although CircleOS itself is not used. */
+*/
 
 /* Standard includes. */
 #include <string.h>
@@ -81,132 +39,135 @@ utilised, although CircleOS itself is not used. */
  * Configure the clocks, GPIO and other peripherals as required by the demo.
  */
 static void prvSetupHardware( void );
-
 static void GPIO_LED_Conf(void);
 
 /*-----------------------------------------------------------*/
 void Task1_LED(void *param)
 {
-	GPIO_LED_Conf();
-	const TickType_t xTickstoWait = pdMS_TO_TICKS(500);
-	while (1)
-	{
-		GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-		vTaskDelay(xTickstoWait);
-		GPIO_SetBits(GPIOC, GPIO_Pin_13);
-		vTaskDelay(xTickstoWait);
-	}
+    GPIO_LED_Conf();
+    const TickType_t xTickstoWait = pdMS_TO_TICKS(500);
+    while (1)
+    {
+        GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+        vTaskDelay(xTickstoWait);
+        GPIO_SetBits(GPIOC, GPIO_Pin_13);
+        vTaskDelay(xTickstoWait);
+    }
 }
 
 void Task2_Print(void *param)
 {
-	USART_Conf();
-	const TickType_t xTickstoWait = pdMS_TO_TICKS(1000);
-	while (1)
-	{
-		printf("Print Task is runing every 1 second...\n");
-		vTaskDelay(xTickstoWait);
-	}
+    USART_Conf();
+    const TickType_t xTickstoWait = pdMS_TO_TICKS(1000);
+    while (1)
+    {
+        printf("Print Task is runing every 1 second...\n");
+        vTaskDelay(xTickstoWait);
+    }
+}
+
+void AppTask_Create(void *param)
+{
+    xTaskCreate(Task1_LED, "Task1_LED", 100, NULL, 1, NULL);
+    xTaskCreate(Task2_Print, "Task2_Print", 100, NULL, 1, NULL);
 }
 
 int main( void )
 {
-	#ifdef DEBUG
-		debug();
-	#endif
+    #ifdef DEBUG
+        debug();
+    #endif
 
-	prvSetupHardware();
+    prvSetupHardware();
 
-	// xTaskCreate(Task1_LED, "Task1_LED", 100, NULL, 1, NULL);
-	// xTaskCreate(Task2_Print, "Task2_Print", 100, NULL, 1, NULL);
+    /* Start the scheduler. */
+    AppTask_Create(NULL);
+    vTaskStartScheduler();
+    // Task1_LED(NULL);
 
-	/* Start the scheduler. */
-	// vTaskStartScheduler();
-	Task1_LED(NULL);
-
-	/* Will only get here if there was not enough heap space to create the
-	idle task. */
-	// while(1)
-	// {
-	// };
-	return 0;
+    /* Will only get here if there was not enough heap space to create the
+    idle task. */
+    while(1)
+    {
+    };
+    return 0;
 }
 /*-----------------------------------------------------------*/
 
 static void GPIO_LED_Conf(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
 /*-----------------------------------------------------------*/
 
 static void prvSetupHardware( void )
 {
-	/* Start with the clocks in their expected state. */
-	RCC_DeInit();
+    /* Start with the clocks in their expected state. */
+    RCC_DeInit();
 
-	/* Enable HSE (high speed external clock). */
-	RCC_HSEConfig( RCC_HSE_ON );
+    /* Enable HSE (high speed external clock). */
+    RCC_HSEConfig( RCC_HSE_ON );
 
-	/* Wait till HSE is ready. */
-	while( RCC_GetFlagStatus( RCC_FLAG_HSERDY ) == RESET )
-	{
-	}
+    /* Wait till HSE is ready. */
+    while( RCC_GetFlagStatus( RCC_FLAG_HSERDY ) == RESET )
+    {
+    }
 
-	/* 2 wait states required on the flash. */
-	*( ( unsigned long * ) 0x40022000 ) = 0x02;
+    /* 2 wait states required on the flash. */
+    *( ( unsigned long * ) 0x40022000 ) = 0x02;
 
-	/* HCLK = SYSCLK */
-	RCC_HCLKConfig( RCC_SYSCLK_Div1 );
+    /* HCLK = SYSCLK */
+    RCC_HCLKConfig( RCC_SYSCLK_Div1 );
 
-	/* PCLK2 = HCLK */
-	RCC_PCLK2Config( RCC_HCLK_Div1 );
+    /* PCLK2 = HCLK */
+    RCC_PCLK2Config( RCC_HCLK_Div1 );
 
-	/* PCLK1 = HCLK/2 */
-	RCC_PCLK1Config( RCC_HCLK_Div2 );
+    /* PCLK1 = HCLK/2 */
+    RCC_PCLK1Config( RCC_HCLK_Div2 );
 
-	/* PLLCLK = 12MHz * 6 = 72 MHz. */
-	RCC_PLLConfig( RCC_PLLSource_HSE_Div1, RCC_PLLMul_6 );
+    /* PLLCLK = 12MHz * 6 = 72 MHz. */
+    RCC_PLLConfig( RCC_PLLSource_HSE_Div1, RCC_PLLMul_6 );
 
-	/* Enable PLL. */
-	RCC_PLLCmd( ENABLE );
+    /* Enable PLL. */
+    RCC_PLLCmd( ENABLE );
 
-	/* Wait till PLL is ready. */
-	while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
-	{
-	}
+    /* Wait till PLL is ready. */
+    while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+    {
+    }
 
-	/* Select PLL as system clock source. */
-	RCC_SYSCLKConfig( RCC_SYSCLKSource_PLLCLK );
+    /* Select PLL as system clock source. */
+    RCC_SYSCLKConfig( RCC_SYSCLKSource_PLLCLK );
 
-	/* Wait till PLL is used as system clock source. */
-	while( RCC_GetSYSCLKSource() != 0x08 )
-	{
-	}
+    /* Wait till PLL is used as system clock source. */
+    while( RCC_GetSYSCLKSource() != 0x08 )
+    {
+    }
 
-	/* Enable GPIOA, GPIOB, GPIOC, GPIOD, GPIOE and AFIO clocks */
-	RCC_APB2PeriphClockCmd(	RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |RCC_APB2Periph_GPIOC
-							| RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_AFIO, ENABLE );
+    /* Enable GPIOA, GPIOB, GPIOC, GPIOD, GPIOE and AFIO clocks */
+    RCC_APB2PeriphClockCmd(    RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |RCC_APB2Periph_GPIOC
+                            | RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_AFIO, ENABLE );
 
-	/* SPI2 Periph clock enable */
-	RCC_APB1PeriphClockCmd( RCC_APB1Periph_SPI2, ENABLE );
+    /* SPI2 Periph clock enable */
+    RCC_APB1PeriphClockCmd( RCC_APB1Periph_SPI2, ENABLE );
 
 
-	/* Set the Vector Table base address at 0x08000000 */
-	NVIC_SetVectorTable( NVIC_VectTab_FLASH, 0x0 );
+    /* Set the Vector Table base address at 0x08000000 */
+    NVIC_SetVectorTable( NVIC_VectTab_FLASH, 0x0 );
 
-	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
+    NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
 
-	/* Configure HCLK clock as SysTick clock source. */
-	SysTick_CLKSourceConfig( SysTick_CLKSource_HCLK );
+    /* Configure HCLK clock as SysTick clock source. */
+    SysTick_CLKSourceConfig( SysTick_CLKSource_HCLK );
 
-	/* Misc initialisation, including some of the CircleOS features.  Note
-	that CircleOS itself is not used. */
+    /* Misc initialisation, including some of the CircleOS features.  Note
+    that CircleOS itself is not used. */
 }
 /*-----------------------------------------------------------*/
 
